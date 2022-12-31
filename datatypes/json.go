2800d0e2cd73ca8a4e25f372b8da1e779ec47469
Jeff Haynie <jhaynie@shopmonkey.io>
2022-12-31 16:10:26 -0600

    Add helpers for JSON

--- datatypes/json.go
+++ datatypes/json.go
@@ -28,6 +28,70 @@ func getJSONDataType(db *gorm.DB) string {
 // JSON defined JSON data type, need to implements driver.Valuer, sql.Scanner interface
 type JSON json.RawMessage
 
+// NewJSONObject returns an empty JSON object
+func NewJSONObject() (JSON, error) {
+	return []byte(`{}`), nil
+}
+
+// NewJSONArray returns an empty JSON array
+func NewJSONArray() (JSON, error) {
+	return []byte(`[]`), nil
+}
+
+// NewJSON returns a JSON structure from a map of key/value pairs
+func NewJSON(kv map[string]interface{}) (JSON, error) {
+	buf, err := json.Marshal(kv)
+	return buf, err
+}
+
+// Get will return the key value from the JSON object
+func (j *JSON) Get(key string) (interface{}, error) {
+	var kv map[string]interface{}
+	if err := json.Unmarshal(*j, &kv); err != nil {
+		return nil, err
+	}
+	return kv[key], nil
+}
+
+// Set will set the key and value in the JSON object
+func (j *JSON) Set(key string, val interface{}) error {
+	var kv map[string]interface{}
+	if err := json.Unmarshal(*j, &kv); err != nil {
+		return err
+	}
+	kv[key] = val
+	newj, err := json.Marshal(kv)
+	if err != nil {
+		return err
+	}
+	*j = newj
+	return nil
+}
+
+// Del will delete the key in the JSON object
+func (j *JSON) Del(key string) error {
+	var kv map[string]interface{}
+	if err := json.Unmarshal(*j, &kv); err != nil {
+		return err
+	}
+	delete(kv, key)
+	newj, err := json.Marshal(kv)
+	if err != nil {
+		return err
+	}
+	*j = newj
+	return nil
+}
+
+// Has returns true if the key exists in the JSON object
+func (j *JSON) Has(key string) bool {
+	var kv map[string]interface{}
+	if err := json.Unmarshal(*j, &kv); err != nil {
+		return false
+	}
+	return kv[key] != nil
+}
+
 // Value return json value, implement driver.Valuer interface
 func (j JSON) Value() (driver.Value, error) {
 	if len(j) == 0 {
