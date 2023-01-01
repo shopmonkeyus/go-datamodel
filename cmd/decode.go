dbca99120197169908db368cdfd8f2a084b95294
Jeff Haynie <jhaynie@shopmonkey.io>
2023-01-01 16:22:33 -0600

    re-enable decode

--- cmd/decode.go
+++ cmd/decode.go
@@ -1,52 +1,56 @@
 package cmd
 
-// import (
-// 	"fmt"
-// 	"io"
-// 	"os"
-// 	"path"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path"
 
-// 	v3 "github.com/shopmonkeyus/go-datamodel/v3"
-// 	"github.com/spf13/cobra"
-// )
+	v3 "github.com/shopmonkeyus/go-datamodel/v3"
+	"github.com/spf13/cobra"
+)
 
-// var decodeCmd = &cobra.Command{
-// 	Use:   "decode [name] [input-file]",
-// 	Short: "Decode a datamodel JSON",
-// 	Long:  `This command decode a datamodel JSON document and validate it`,
-// 	Run: func(cmd *cobra.Command, args []string) {
-// 		if len(args) == 0 {
-// 			cmd.Help()
-// 			os.Exit(0)
-// 		}
-// 		name := args[0]
-// 		fn := args[1]
-// 		if _, err := os.Stat(fn); err != nil {
-// 			fmt.Printf("File %s doesn't exist\n", fn)
-// 			os.Exit(1)
-// 		}
-// 		file, err := os.Open(fn)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			os.Exit(1)
-// 		}
-// 		defer file.Close()
-// 		buf, err := io.ReadAll(file)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			os.Exit(1)
-// 		}
-// 		m, err := v3.NewFromChangeEvent(name, buf, path.Ext(fn) == ".gz")
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			os.Exit(1)
-// 		}
-// 		fmt.Println(m)
-// 		// j, err := json.MarshalIndent(m, "", " ")
-// 		// fmt.Println(string(j), err)
-// 	},
-// }
+var decodeCmd = &cobra.Command{
+	Use:   "decode [name] [input-file]",
+	Short: "Decode a change event JSON",
+	Long:  `This command decode a change event JSON document and validate it`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+		name := args[0]
+		fn := args[1]
+		if _, err := os.Stat(fn); err != nil {
+			fmt.Printf("File %s doesn't exist\n", fn)
+			os.Exit(1)
+		}
+		file, err := os.Open(fn)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		buf, err := io.ReadAll(file)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		m, err := v3.NewFromChangeEvent(name, buf, path.Ext(fn) == ".gz")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		j, err := json.MarshalIndent(m, "", " ")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(string(j))
+	},
+}
 
-// func init() {
-// 	rootCmd.AddCommand(decodeCmd)
-// }
+func init() {
+	rootCmd.AddCommand(decodeCmd)
+}
--- go.mod
+++ go.mod
@@ -9,9 +9,9 @@ require (
 )
 
 require (
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	gorm.io/gorm v1.24.2
+	gorm.io/gorm v1.24.3
 )
